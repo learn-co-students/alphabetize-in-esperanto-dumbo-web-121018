ed4be5dff8fe92d97d67622fb587ec0c524b8f0f
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-09 20:40:54 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |sentence| 
+    sentence.split("").collect do |char| 
+      alphabet.index(char) 
+    end
+  end 
 end
